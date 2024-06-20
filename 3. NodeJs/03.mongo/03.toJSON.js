f60344ccdd3f38c107113e3506654861122ae50f
Martin Wilches <martindotdev@gmail.com>
2024-06-20 11:12:04 -0500

    Formatear objetos retornados por MongoDB con toJSON

---
+++ 3. NodeJs/03.mongo/03.toJSON.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://martindotdev:${password}@fullstackopencluster.asrmplj.mongodb.net/?retryWrites=true&w=majority&appName=FullStackOpenCluster`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteSchema = mongoose.Schema({
+    content: String,
+    important: Boolean
+})
+
+/**
+ * Los objetos devueltos por mongoose pueden ser modificados mediante el metodo `toJson` del esquema utilizado por todas las instancias de los modelos producidos por ese esquema.
+ * En el siguiente ejemplo se añade una nueva propiedad id al objeto retornado, a partir de la propiedad _id la cual es formateada a tipo string.
+ * Se elimnan ademas las propiedades retornadas por el objeto _id y __v.
+ * 
+*/
+noteSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString(),
+        delete returnedObject._id,
+        delete returnedObject.__v
+    }
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const notes = new Note({
+    content: "HTML is easy",
+    important: true
+})
