a64f36163ac008e8d565b0fc5b734e6ccfa337a6
Martin Wilches <martindotdev@gmail.com>
2024-11-09 22:00:05 -0500

    Parte 3, proyecto phonebook crud basico

---
+++ part_3/exercises/phonebook/index.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const app = express()
+
+const PORT = 3001
+
+let persons = [
+    {
+        "id": "1",
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": "2",
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": "3",
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": "4",
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+
+app.use(express.json())
+
+app.get('/api/persons', (req, res) => {
+    res.json({persons: persons})
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const person = persons.find(person => Number(person.id) === Number(req.params.id))
+    if (person) {
+        res.json({person: person})
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => Number(person.id) === id)
+    if (person.length > 0) {
+        persons = persons.filter(person => Number(person.id) !== Number(id))
+        res.status(204).end()
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.post('/api/persons', (req, res) => {
+    const id = Number(persons[persons.length -1]?.id) || 1
+    const body = req.body
+
+    if (!body.name || !body.number) {
+        res.status(400).json({"error": "Invalid name or number"})
+    } else {
+        const newPerson = {
+            id: id + 1,
+            name: body.name,
+            number: body.number
+        }
+
+        if (persons.find(person => person.name === body.name)) {
+            res.status(409).json({"error": "name must be unique"})
+        } else {
+            persons = [...persons, newPerson]
+            res.status(201).end()
+        }
+    }
+})
+
+app.get('/info', (req, res) => {
+    res.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date()}</p>
+    `)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
